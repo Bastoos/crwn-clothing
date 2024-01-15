6678ef37056c3b8625a97ca45d768abb02056f04
Venâncio Bastos <vjpb2002@gmail.com>
2024-01-15 23:09:26 +0000

    Adding GoogleRedirect Singin

--- src/routes/sign-in/SignIn.js
+++ src/routes/sign-in/SignIn.js
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
 import {
   createUserDocumentFromAuth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  auth,
 } from "../../utils/firebase/Firebase";
+import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getRedirectResult(auth);
+
+      if (res) {
+        const userDocRef = await createUserDocumentFromAuth(res.user);
+      }
+
+      console.log(res);
+    };
+
+    fetchData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
 
@@ -16,6 +34,9 @@ const SignIn = () => {
     <div>
       <h1>Sign-in</h1>
       <button onClick={logGoogleUser}>Sign-in with google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign-in with google Redirect
+      </button>
     </div>
   );
 };
